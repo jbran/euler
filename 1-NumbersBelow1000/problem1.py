afbc93e9a3e06ea5b06ac026094c5051f5f99427
John <jrbran+github@gmail.com>
2011-04-09 19:22:13 -0600

    Learnings from the euler recap

--- 1-NumbersBelow1000/problem1.py
+++ 1-NumbersBelow1000/problem1.py
@@ -1,5 +1,15 @@
 
 
+def go(n):
+    target = n
+    return sumDiv(3,target) + sumDiv(5,target) - sumDiv(15,target)
+
+def sumDiv(n, target):
+#The max value to sum to
+    max_value = target // n
+#Where the summation of 1...N = n(n+1)  / 2
+    return n*(max_value*(max_value+1)) // 2
+
 def sumOf3and5(n):
     sum = 0;
     for x in xrange(1,n):
@@ -9,5 +19,7 @@ def sumOf3and5(n):
 
 
 
-print sumOf3and5(10)
-print sumOf3and5(1000)
+#print sumOf3and5(10)
+#print sumOf3and5(1000)
+#print sumOf3and5(1000000000)
+print go(1000000000)
