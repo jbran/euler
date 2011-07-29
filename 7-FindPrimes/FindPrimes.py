20aaf8202ab805dd583f105792f01350d26b3eb6
John <john@doop.(none)>
2011-07-28 20:00:42 -0600

    Limit the prime checking only up to sqrt(n).

--- 7-FindPrimes/FindPrimes.py
+++ 7-FindPrimes/FindPrimes.py
@@ -1,10 +1,11 @@
+import math
 # Our initial seed of prime numbers up to 99
 prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 def findNthPrime(n):
-    i = 100
+    i = 99
     while len(prime) < n:
-        i += 1
+        i += 2
         if checkIfPrime(i):
             prime.append(i)
     return prime
@@ -15,7 +16,10 @@ Go through our list of primes and see if an input, n, is divisible by that prime
 
 """
 def checkIfPrime(n):
+    sqrt = math.floor(math.sqrt(n))
     for p in prime:
+        if p > sqrt:
+            break;
         if (n % p) == 0:
             return False
     return True
